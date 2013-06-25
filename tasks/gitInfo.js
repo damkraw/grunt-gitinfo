599040953a817fdb537114e79162f7fae3490171
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2013-06-25 18:28:22 -0300

    Make cwd actually work

--- tasks/gitInfo.js
+++ tasks/gitInfo.js
@@ -11,99 +11,118 @@
 module.exports = function(grunt) {
     grunt.registerTask('gitinfo', 'Your task description goes here.', function () {
         var done = this.async(),
-            gitinfo = {},
+        gitinfo = grunt.config.get('gitinfo'),
 
-            getCurrentBranchName = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['rev-parse', '--abbrev-ref', 'HEAD']
-                }, function (err, result) {
-                  if (err) {
-                      done();
-                  } else {
-                      gitinfo.local.branch.current.name = result.stdout;
-                      grunt.config.set('gitinfo', gitinfo);
-                      done();
-                  }
-                });
-            },
+        getCurrentBranchName = function () {
+            grunt.util.spawn({
+                cmd : 'git',
+                args : ['rev-parse', '--abbrev-ref', 'HEAD'],
+                opts : {
+                    cwd : gitinfo.options.cwd
+                }
+            }, function (err, result) {
+                if (err) {
+                    done();
+                } else {
+                    gitinfo.local.branch.current.name = result.stdout;
+                    grunt.config.set('gitinfo', gitinfo);
+                    done();
+                }
+            });
+        },
 
-            getRemoteOriginUrl = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['config', '--get-all', 'remote.origin.url']
-                }, function (err, result) {
-                    if (err) {
-                        done();
-                    } else {
-                        gitinfo.remote = {
-                            origin : {
-                                url : result.stdout
-                            }
-                        };
-                        getCurrentBranchName();
-                    }
-                });
-            },
-            
-            getLastCommitTime = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['log', '--format="%ai"', '-n1', 'HEAD']
-                }, function (err, result) {
-                    if (err) {
-                        done();
-                    } else {
-                        gitinfo.local.branch.current.lastCommitTime = result.stdout;
-                        getRemoteOriginUrl();
-                    }
-                });
-            },
-            getLastCommitAuthor = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['log', '--format="%aN"', '-n1', 'HEAD']
-                }, function (err, result) {
-                    if (err) {
-                        done();
-                    } else {
-                        gitinfo.local.branch.current.lastCommitAuthor = result.stdout;
-                        getLastCommitTime();
-                    }
-                });
-            },
-            getShortSHA = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['rev-parse', '--short', 'HEAD']
-                }, function (err, result) {
-                    if (err) {
-                        done();
-                    } else {
-                        gitinfo.local.branch.current.shortSHA = result.stdout;
-                        getLastCommitAuthor();
-                    }
-                });
-            },
-            getSHA = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['rev-parse', 'HEAD']
-                }, function (err, result) {
-                    if (err) {
-                        done();
-                    } else {
-                        gitinfo.local = {
-                           branch : {
-                               current : {
-                                   SHA : result.stdout
-                               }
+        getRemoteOriginUrl = function () {
+            grunt.util.spawn({
+
+                cmd : 'git',
+                args : ['config', '--get-all', 'remote.origin.url'],
+                opts : {
+                    cwd : gitinfo.options.cwd
+                }
+            }, function (err, result) {
+                if (err) {
+                    done();
+                } else {
+                    gitinfo.remote = {
+                        origin : {
+                            url : result.stdout
+                        }
+                    };
+                    getCurrentBranchName();
+                }
+            });
+        },
+
+        getLastCommitTime = function () {
+            grunt.util.spawn({
+                cmd : 'git',
+                args : ['log', '--format="%ai"', '-n1', 'HEAD'],
+                opts : {
+                    cwd : gitinfo.options.cwd
+                }
+            }, function (err, result) {
+                if (err) {
+                    done();
+                } else {
+                    gitinfo.local.branch.current.lastCommitTime = result.stdout;
+                    getRemoteOriginUrl();
+                }
+            });
+        },
+        getLastCommitAuthor = function () {
+            grunt.util.spawn({
+                cmd : 'git',
+                args : ['log', '--format="%aN"', '-n1', 'HEAD'],
+                opts : {
+                    cwd : gitinfo.options.cwd
+                }
+            }, function (err, result) {
+                if (err) {
+                    done();
+                } else {
+                    gitinfo.local.branch.current.lastCommitAuthor = result.stdout;
+                    getLastCommitTime();
+                }
+            });
+        },
+        getShortSHA = function () {
+            grunt.util.spawn({
+                cmd : 'git',
+                args : ['rev-parse', '--short', 'HEAD'],
+                opts : {
+                    cwd : gitinfo.options.cwd
+                }
+            }, function (err, result) {
+                if (err) {
+                    done();
+                } else {
+                    gitinfo.local.branch.current.shortSHA = result.stdout;
+                    getLastCommitAuthor();
+                }
+            });
+        },
+        getSHA = function () {
+            grunt.util.spawn({
+                cmd : 'git',
+                args : ['rev-parse', 'HEAD'],
+                opts : {
+                    cwd : gitinfo.options.cwd
+                }
+            }, function (err, result) {
+                if (err) {
+                    done();
+                } else {
+                    gitinfo.local = {
+                       branch : {
+                           current : {
+                               SHA : result.stdout
                            }
-                        };
-                        getShortSHA();
-                    }
-                });
-            };
+                       }
+                    };
+                    getShortSHA();
+                }
+            });
+        };
         getSHA();
     });
 };
