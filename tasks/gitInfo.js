4fe08b144f6f71fb6edbe8a6815ea00effaf3fbc
Corey Jewett <cj@syntheticplayground.com>
2013-07-08 22:11:55 -0700

    Update gitInfo.js
    Run git commands in parallel. Added support for --debug and --verbose.

--- tasks/gitInfo.js
+++ tasks/gitInfo.js
@@ -9,75 +9,51 @@
 'use strict';
 
 module.exports = function(grunt) {
-    grunt.registerTask('gitinfo', 'Your task description goes here.', function () {
-        var done = this.async(),
-            gitinfo = {},
+  grunt.registerTask('gitinfo', 'Your task description goes here.', function () {
+    var done = this.async(),
+    gitinfo = {
+      local: {
+        branch : {
+          current : {}
+        }
+      },
+      remote: {
+        origin: {}
+      }
+    },
+    commands = {
+      'local.branch.current.name': ['rev-parse', '--abbrev-ref', 'HEAD'],
+      'remote.origin.url': ['config', '--get-all', 'remote.origin.url'],
+      'local.branch.current.SHA': ['rev-parse', 'HEAD'],
+      'local.branch.current.shortSHA': ['rev-parse', '--short', 'HEAD'],
+    };
 
-            getCurrentBranchName = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['rev-parse', '--abbrev-ref', 'HEAD']
-                }, function (err, result) {
-                  if (err) {
-                      done();
-                  } else {
-                      gitinfo.local.branch.current.name = result.stdout;
-                      grunt.config.set('gitinfo', gitinfo);
-                      done();
-                  }
-                });
-            },
+    function work(conf_key, cb) {
+      var spawn_args = commands[conf_key];
 
-            getRemoteOriginUrl = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['config', '--get-all', 'remote.origin.url']
-                }, function (err, result) {
-                    if (err) {
-                        console.warn("Git: couldn't get config: remote.origin.url");
-                    } else {
-                        gitinfo.remote = {
-                            origin : {
-                                url : result.stdout
-                            }
-                        };
-                        getCurrentBranchName();
-                    }
-                });
-            },
+      grunt.util.spawn({
+          cmd: 'git',
+          args: spawn_args},
+        function (err, result) {
+          if (err) {
+            console.warn("[gitinfo]: couldn't set config:", conf_key);
+          } else {
+            gitinfo[conf_key] = result.stdout;
 
-            getShortSHA = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['rev-parse', '--short', 'HEAD']
-                }, function (err, result) {
-                    if (err) {
-                        done();
-                    } else {
-                        gitinfo.local.branch.current.shortSHA = result.stdout;
-                        getRemoteOriginUrl();
-                    }
-                });
-            },
-            getSHA = function () {
-                grunt.util.spawn({
-                    cmd : 'git',
-                    args : ['rev-parse', 'HEAD']
-                }, function (err, result) {
-                    if (err) {
-                        done();
-                    } else {
-                        gitinfo.local = {
-                           branch : {
-                               current : {
-                                   SHA : result.stdout
-                               }
-                           }
-                        };
-                        getShortSHA();
-                    }
-                });
-            };
-        getSHA();
-    });
-};
+            if (grunt.option("debug") || grunt.option("verbose")) {
+              console.log("[gitinfo]:", conf_key, "=", gitinfo[conf_key]);
+            }
+          }
+
+          cb();
+        });
+    }
+
+    function fin() {
+      grunt.config.set('gitinfo', gitinfo);
+      done();
+    }
+
+    grunt.util.async.forEach(grunt.util._.keys(commands), work, fin);
+  });
+}
