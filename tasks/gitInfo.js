34f70e55eb6df0746fb2427d36d24a8436ee8913
Damian Krawczyk <mail@damiankrawczyk.pl>
2014-01-07 22:57:19 +0100

    c2h5oh changes: - last commit author - last commit time - commit tag - username - cwd
    cs

--- tasks/gitInfo.js
+++ tasks/gitInfo.js
@@ -2,61 +2,71 @@
  * grunt-gitinfo
  * https://github.com/damkraw/grunt-gitinfo
  *
- * Copyright (c) 2013 Damian Krawczyk
+ * Copyright (c) 2013 Damian Krawczyk, Maciej Lisiewski
  * Licensed under the MIT license.
  */
 
-'use strict';
+// JSLint
+/*global module, console*/
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+    'use strict';
     grunt.registerTask('gitinfo', 'Your task description goes here.', function () {
         var done = this.async(),
-        gitinfo = {},
-        commands = {
-            'local.branch.current.name'     : ['rev-parse', '--abbrev-ref', 'HEAD'],
-            'local.branch.current.SHA'      : ['rev-parse', 'HEAD'],
-            'local.branch.current.shortSHA' : ['rev-parse', '--short', 'HEAD'],
-            'remote.origin.url'             : ['config', '--get-all', 'remote.origin.url']
-        };
+            gitinfo = {},
+            config = grunt.config.get('gitinfo'),
+            commands = {
+                'local.branch.current.name'             : ['rev-parse', '--abbrev-ref', 'HEAD'],
+                'local.branch.current.SHA'              : ['rev-parse', 'HEAD'],
+                'local.branch.current.shortSHA'         : ['rev-parse', '--short', 'HEAD'],
+                'local.branch.current.currentUser'      : ['config', '--global', 'user.name'],
+                'local.branch.current.lastCommitTime'   : ['log', '--format="%ai"', '-n1', 'HEAD'],
+                'local.branch.current.lastCommitAuthor' : ['log', '--format="%aN"', '-n1', 'HEAD'],
+                'local.branch.current.tag'              : ['describe', '--abbrev=0', '--exact-match'],
+                'remote.origin.url'                     : ['config', '--get-all', 'remote.origin.url']
+            },
 
-        function work(conf_key, cb) {
-            var spawn_args = commands[conf_key];
+            work = function (conf_key, cb) {
+                var spawn_args = commands[conf_key];
 
-            grunt.util.spawn(
-                {
-                    cmd  : 'git',
-                    args : spawn_args
-                },
-                function (err, result) {
-                    if (err) {
-                        console.warn("[gitinfo]: couldn't set config:", conf_key);
-                    } else {
-                        var ref = gitinfo, keys = conf_key.split(".");
-                        for (var i = 0; i < keys.length-1; i++) {
-                            var key = keys[i];
+                grunt.util.spawn(
+                    {
+                        cmd  : 'git',
+                        args : spawn_args,
+                        opts : {
+                            cwd : (config.options && config.options.cwd) ? config.options.cwd : null
+                        }
+                    },
+                    function (err, result) {
+                        if (err) {
+                            console.warn("[gitinfo]: couldn't set config:", conf_key);
+                        } else {
+                            var ref = gitinfo, keys = conf_key.split("."), i, key;
+                            for (i = 0; i < keys.length - 1; i += 1) {
+                                key = keys[i];
 
-                            if (ref[key] === undefined) {
-                                ref[key] = {};
+                                if (ref[key] === undefined) {
+                                    ref[key] = {};
+                                }
+
+                                ref = ref[key];
                             }
 
-                            ref = ref[key];
-                        }
+                            ref[keys.pop()] = result.stdout;
 
-                        ref[keys.pop()] = result.stdout;
-
-                        if (grunt.option("debug") || grunt.option("verbose")) {
-                            console.log("[gitinfo]:", conf_key, "=", result.stdout);
+                            if (grunt.option("debug") || grunt.option("verbose")) {
+                                console.log("[gitinfo]:", conf_key, "=", result.stdout);
+                            }
                         }
+                        cb();
                     }
+                );
+            },
 
-                    cb();
-            });
-        }
-
-        function fin() {
-            grunt.config.set('gitinfo', gitinfo);
-            done();
-        }
+            fin = function () {
+                grunt.config.set('gitinfo', gitinfo);
+                done();
+            };
 
         grunt.util.async.forEach(grunt.util._.keys(commands), work, fin);
     });
